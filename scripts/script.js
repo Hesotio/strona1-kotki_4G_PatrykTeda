f249fbce617e7f47172bb315e208be84dff898bf
Patryk Teda <104190971+Hesotio@users.noreply.github.com>
2022-11-06 19:56:43 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+function showCat1(){
+    $("article").removeClass("active");
+    $("article:nth-child(1)").addClass("active");
+}
+
+function showCat2(){
+    $("article").removeClass("active");
+    $("article:nth-child(2)").addClass("active");
+}
+
+function showCat3(){
+    $("article").removeClass("active");
+    $("article:nth-child(3)").addClass("active");
+}
+
+function showCat4(){
+    $("article").removeClass("active");
+    $("article:nth-child(4)").addClass("active");
+}
+
+function showCat5(){
+    $("article").removeClass("active");
+    $("article:nth-child(5)").addClass("active");
+}
+
+function showCat6(){
+    $("article").removeClass("active");
+    $("article:nth-child(6)").addClass("active");
+}
